90119eed7ac25a2b33c9780f889fff9238bbf81c
B-Selin <selin@bozkurt.ca>
2023-07-04 15:57:10 -0600

    remove unneccessary files

--- controllers/jokes.js
+++ controllers/jokes.js
@@ -10,20 +10,16 @@ module.exports = {
 // Export function to get a random dad joke 
 //from the dad jokes API
 
+module.exports = {
+  index
+};
+
 function index(req, res) {
-  // Fetch a random dad joke from the API
-  fetch(dadJokesURL)
-    .then((res => res.json())
-    //Extract the joke from the response JSON 
-      .then(joke => { 
-        res.render(index, { 
-          joke: joke.value 
-        }) 
-      })
-    )
-    // Send a 500 error if unable to fetch a joke, not sure if necessary
-    .catch(err => {
-      console.log(err);
-      res.status(500).send('An unexpected error occurred');
+  fetch(`${dadJokesURL}`)
+    .then(res => res.json())
+    .then(joke => {
+      res.render('index', {
+        joke:joke.value
+      });
     });
-}
+};
